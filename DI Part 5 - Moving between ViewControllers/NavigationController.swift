197dc1c2cfbddcd861490ffd3bf457a0273307f5
Red Wang <redxoxored@gmail.com>
2023-11-02 14:21:03 +0800

    feat: Set up root view controller as NavigationController

--- DI Part 5 - Moving between ViewControllers/NavigationController.swift
+++ DI Part 5 - Moving between ViewControllers/NavigationController.swift
@@ -5,4 +5,17 @@
 //  Created by Red Wang on 2023/11/2.
 //
 
-import Foundation
+import UIKit
+
+class NavigationController: UINavigationController {
+    
+    static var shared: UINavigationController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let navBarAppearance = UINavigationBarAppearance()
+        self.navigationBar.standardAppearance = navBarAppearance
+        self.navigationBar.scrollEdgeAppearance = navBarAppearance
+        self.modalPresentationStyle = .fullScreen
+    }
+}
