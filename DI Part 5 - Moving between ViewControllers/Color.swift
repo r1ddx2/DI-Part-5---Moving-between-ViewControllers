6810cddd3c5c85b6dad2df00252371a2afc8bf63
Red Wang <redxoxored@gmail.com>
2023-11-02 14:20:22 +0800

    feat: Add Color to distinguish

--- DI Part 5 - Moving between ViewControllers/Color.swift
+++ DI Part 5 - Moving between ViewControllers/Color.swift
@@ -5,4 +5,26 @@
 //  Created by Red Wang on 2023/11/1.
 //
 
-import Foundation
+import UIKit
+enum Color: String, CaseIterable {
+    case red
+    case orange
+    case yellow
+    case green
+    case blue
+    
+    var backgroundColor: UIColor {
+        switch self {
+        case .red:
+            return .red
+        case .orange:
+            return .orange
+        case .yellow:
+            return .yellow
+        case .green:
+            return .green
+        case .blue:
+            return .blue
+        }
+    }
+}
--- DI Part 5 - Moving between ViewControllers/ViewController + Extension.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UIViewController + Extension.swift
-//  DI Part 5 - Moving between ViewControllers
-//
-//  Created by Red Wang on 2023/11/1.
-//
-
-import UIKit
-
